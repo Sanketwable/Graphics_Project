3a2dccfc94ff196dd6c92dec6ed3895b8ab2c267
Sanket Wable <43716242+Sanketwable@users.noreply.github.com>
2019-10-28 23:56:40 +0530

    Updated

--- main.cpp
+++ main.cpp
@@ -6,15 +6,6 @@
 using namespace std;
 
 
-
-GLsizei wh=600, ww=800, wx, wy, wx1=800, wy1=600;
-int count=0,sub_menu,draw,i,j,k,m,n,pol,flag=0,flag1,fill=0,size=1,save=0;
-float a[100][2],b[100][2],textx,texty,textz=0.0,a1=0,a2=0,a3=0,b1=0,b2=0,b3=0;
-char* image;
-int main() {
-}
-=======
-
 FILE *fptr;
 char fname[20];
 int s=0,wel=0;
