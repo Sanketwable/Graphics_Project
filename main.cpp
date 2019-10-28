b3f6060f18cd5472fc2f69170d1205e9416b27d2
sanket <sanketwable123@gmail.com>
2019-10-28 13:49:29 +0530

    changes

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include<iostream>
 #include<GL/glut.h>
 #include<math.h>
+#include "display.cpp"
+#include "constant.cpp"
 using namespace std;
 
 
-GLsizei wh=600, ww=800, wx, wy, wx1=800, wy1=600;
-
-int count=0,sub_menu,draw,i,j,k,m,n,pol,flag=0,flag1,fill=0,size=1,save=0;
-
-float a[100][2],b[100][2],textx,texty,textz=0.0,a1=0,a2=0,a3=0,b1=0,b2=0,b3=0,posx,posy;
-
-char* image;
-
-void *currentfont;
 
 FILE *fptr;
 char fname[20];
@@ -50,8 +43,6 @@ void scanfill(GLint num1, GLint num2);
 
 void detect_point(GLint num1, GLint num2, int x, int y);
 
-void display(void);
-
 void keys(unsigned char key, int x, int y);
 
 void myMouse(int btn, int state, int x, int  y);
@@ -63,10 +54,16 @@ void point_size(int id);
 void right(int id);
 
 int main(int argc, char **argv) {
-    image = new char;
+    
+    image = new char[3*1450*900*sizeof(char];
     
     glutInit(&argc,argv);
-    
+    glutInitDisplayMode(GLUT_SINGLE| GLUT_RGB);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(ww,wh);
+    glutCreateWindow("G-Edit --Wable Sanket--Avinash Yadav--Shubhang Bhagat");
+    glutDisplayFunc(display);
+    glutMainLoop();
 
 
 }
---
+++ variables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<GL/glut.h>
+
+
+
+GLsizei wh=600, ww=800, wx, wy, wx1=800, wy1=600;
+
+int count=0,sub_menu,draw,i,j,k,m,n,pol,flag=0,flag1,fill=0,size=1,save=0;
+
+float a[100][2],b[100][2],textx,texty,textz=0.0,a1=0,a2=0,a3=0,b1=0,b2=0,b3=0,posx,posy;
+
+char* image;
+
+void *currentfont;
+
