e35885ee1d706b4d1e903ff9114e9808ad07cb7e
sanket <sanketwable123@gmail.com>
2019-11-01 23:22:30 +0530

    Added Initial Screen

--- display.cpp
+++ display.cpp
@@ -33,8 +33,22 @@ void display() {
     
     setFont(GLUT_BITMAP_HELVETICA_18);
     glColor3f(0,1,1);
-    drawstring(22*wh/45,40*wh/45,0.0,"Welcome to Graphics Editor");
-
+    drawstring(-0.17,0.8,0.0,"Graphics Editor");
+    setFont(GLUT_BITMAP_TIMES_ROMAN_10);
+    drawstring(-0.05,0.75,0.0,"by");
+    setFont(GLUT_BITMAP_TIMES_ROMAN_24);
+    glColor3f(1,1,1);
+    drawstring(-0.9,0.4,0.0,"Wable Sanket - 185088");
+    drawstring(-0.9,0.25,0.0,"Avinash Yadav - 185058");
+    drawstring(-0.9,0.1,0.0,"Shubhang Bhagat - 185060");
+    setFont(GLUT_BITMAP_HELVETICA_18);
+    glColor3f(0.5,0.5,1.0);
+    drawstring(-0.5,-0.2,0.0,"Computer Science and Engineering Department");
+    drawstring(-0.2,-0.3,0.0,"NIT Hamirpur");
+    glColor3f(1.0,1.0,1.0);
+    drawstring(0.4,-0.65,0.0,"Under the guidance of");
+    setFont(GLUT_BITMAP_TIMES_ROMAN_24);
+    drawstring(0.4,-0.8,0.0,"Dr. Naveen Chauhan");
     }
     glFlush();
     glClearColor(1.0,1.0,1.0,1.0);
