c141eedf944a5cbcb8f69ab756f1ce9d579afe32
sanket <sanketwable123@gmail.com>
2019-11-02 23:57:38 +0530

    adding mouse

--- mouse.cpp
+++ mouse.cpp
@@ -8,8 +8,21 @@ using namespace std;
 
 void myMouse(int btn, int state, int x, int  y) {
 
+    GLfloat r,r1,r2;
+    int num1=0,num2=0;
+
+    int i1, ii2;
+    int flag2;
+
+
+    if (btn==GLUT_LEFT_BUTTON && state==GLUT_DOWN) {
+        if (wel==1) {
+            wel=2;
+            display();
+        }
+    }
 
 
 
-    
+
 }
