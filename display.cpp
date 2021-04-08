343a477d272a1d1f40a45d3f822095f5fc80b47e
sanket <sanketwable123@gmail.com>
2021-04-08 14:02:41 +0530

    Latest commit

--- display.cpp
+++ display.cpp
@@ -44,10 +44,9 @@ void First_page() {
     drawstring(-0.05,0.75,0.0,"by");
     setFont(GLUT_BITMAP_TIMES_ROMAN_24);
     glColor3f(1,1,1);
-    drawstring(-0.9,0.4,0.0,"Wable Sanket - 185088");
-    drawstring(-0.9,0.25,0.0,"Avinash Yadav - 185058");
-    drawstring(-0.9,0.1,0.0,"Shubhang Bhagat - 185060");
-    drawstring(-0.9,-0.05,0.0,"Sachin Wattamwar - 185070");
+    drawstring(-0.9,0.4,0.0,"Shubhang Bhagat - 185060");
+    drawstring(-0.9,0.25,0.0,"Harsimranjeet Saini - 185087");
+    drawstring(-0.9,0.1,0.0,"Wable Sanket - 185088");
     glColor3f(1,0,0);
     drawstring(0.3,0.3,0.0,"CSE-4yr");
     setFont(GLUT_BITMAP_HELVETICA_18);
@@ -72,7 +71,7 @@ void Second_page() {
     glColor3f(1.0,1.0,1.0);
     drawstring(-0.2,0.15,0.0,"Under the guidance of");
     setFont(GLUT_BITMAP_HELVETICA_18);
-    drawstring(-0.2,0.0,0.0,"Dr. Naveen Chauhan");
+    drawstring(-0.2,0.0,0.0,"Dr. Dharmendra Prasad Mahato");
 
     glFlush();
     wel=2;
